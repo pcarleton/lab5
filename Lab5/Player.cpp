9118232ecb624971c03101594b2a332f2799b0b0
bfink <bfink@shedwebdesign.com>
2011-04-24 18:39:16 -0500

    Added functionality for multiple players, still need to add main while loop condition and player prompt about new tiles

--- Lab5/Player.cpp
+++ Lab5/Player.cpp
@@ -10,3 +10,24 @@ Player::Player(string & n):name(n),score(0){
 bool Player::operator ==(const Player &other) const{
 	return other.name == name;
 }
+int Player::numTiles(){
+	return tileCollection.size();
+}
+void Player::receiveTiles(LetterTileCollection & bag, int n) {
+	tileCollection.move(bag, n);
+}
+void Player::showTiles(ostream & os) {
+	tileCollection.print(os);
+}
+int Player::getScore() {
+	return score;
+}
+void Player::addScore(int s) {
+	score += s;
+}
+int Player::checkString(string & input, LetterTileCollection & other) {
+	return tileCollection.subCopy(input, other);
+}
+string Player::getName() {
+	return name;
+}
